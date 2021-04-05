4ea19b2093e10109963eaa0f25c7a8d7eda2fd56
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2021-04-04 22:02:12 -0500

    Added week 11 example code

---
+++ Week_11_Example_Code/Assets/Week10_State/Scripts/2. State Pattern - GameObjects/Controllers/DisplayText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StatePatternGameObjects
+{
+    //Attach this class to a UI text object to display the text
+    public class DisplayText : MonoBehaviour
+    {
+        //need to drag these objects into these reference in the inspector
+        public EnemyAIStateManager enemyAIStateManager;
+        public EnemyController enemyController;
+
+
+        private string textToDisplay;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.textToDisplay = gameObject.GetComponent<Text>().text;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            this.textToDisplay = "";
+
+            this.textToDisplay += "Current Enemy State: " + enemyAIStateManager.currentState + "\n";
+
+            this.textToDisplay += "Distance from player: " + enemyController.distanceFromPlayer + ".\n";
+            gameObject.GetComponent<Text>().text = this.textToDisplay;
+        }
+    }
+}
---
+++ Week_11_Example_Code/Assets/Week10_State/Scripts/2. State Pattern - GameObjects/Controllers/EnemyController.cs
@@ -0,0 +1,88 @@
+﻿// This script is from this Brackeys video:https://www.youtube.com/watch?v=blPglabGueM
+
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+namespace StatePatternGameObjects
+{
+    public class EnemyController : MonoBehaviour
+    {
+
+        public Camera cam;
+
+        public NavMeshAgent agent;
+
+        public ThirdPersonCharacter character;
+
+        public Transform target;
+        public Vector3 startPosition;
+
+        public EnemyAIStateManager enemyAIStateManager;
+
+        public float detectionRange;
+        public float distanceFromPlayer;
+        public float distanceFromStart;
+
+        void Start()
+        {
+            agent.updateRotation = false;
+
+            startPosition = transform.position;
+
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            distanceFromPlayer = Vector3.Distance(target.position, transform.position);
+            /* See #16 at https://www.youtube.com/watch?v=thA3zv0IoUM for a faster way to calculate distance
+               than Vector3.Distance if performance is an issue. */
+            
+            distanceFromStart = Vector3.Distance(startPosition, transform.position);
+
+            //If player is within detection range, enemy moves towards player
+            if (distanceFromPlayer <= detectionRange)
+            {
+                //Instead of doing this here...
+                //agent.SetDestination(target.position);
+
+                enemyAIStateManager.currentState.StartChasing();
+
+                //If player is within attack range, enemy attacks player with charge attack
+                if (distanceFromPlayer <= 1.2f)
+                {
+                    Debug.Log("Enemy is close enough to attack");
+                    StartCoroutine(enemyAIStateManager.currentState.ChargeAttack());
+                }
+
+            }
+            //Else if player is outside detection range and not at start position, enemy returns to start position
+            else if (distanceFromStart > 1.2f && (enemyAIStateManager.currentState != enemyAIStateManager.returnToStartState) )
+            {
+                enemyAIStateManager.currentState.StopChasingAndReturnToStart();
+            }
+
+            //Else if player is at start position, change to idle state
+            else if (distanceFromStart <= 1.2f && (enemyAIStateManager.currentState != enemyAIStateManager.idleState) )
+            {
+                enemyAIStateManager.currentState.ReturnedAndReady();
+            }
+
+                       
+
+            //Move towards agent's destination using ThirdPersonCharacter.Move()
+            if (agent.remainingDistance > 1.2f)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+
+        }
+    }
+}
