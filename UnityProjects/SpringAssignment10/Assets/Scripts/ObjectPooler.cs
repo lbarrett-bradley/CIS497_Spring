5cd2ed525b1bcb7e6f8cc787e0ad8595ddf0618a
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2021-04-16 16:02:24 -0500

    Created ObjectPooler and Pool scripts

---
+++ UnityProjects/SpringAssignment10/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    
+}
