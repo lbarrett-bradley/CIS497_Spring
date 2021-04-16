63f95dae4c409edb2ee3cf45c9a698dd5df1052e
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2021-04-16 16:31:07 -0500

    Completed ObjectPooler script

--- UnityProjects/SpringAssignment10/Assets/Scripts/ObjectPooler.cs
+++ UnityProjects/SpringAssignment10/Assets/Scripts/ObjectPooler.cs
@@ -36,20 +36,47 @@ public class ObjectPooler : MonoBehaviour
     void Start()
     {
         poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        FillPoolsWithInactiveObjects();
     }
 
     public void FillPoolsWithInactiveObjects()
     {
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
 
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
     }
 
     public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
     {
-        return null;
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't exist");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        ReturnObjectToPool(tag, objectToSpawn);
+
+        return objectToSpawn;
     }
 
     public void ReturnObjectToPool(string tag, GameObject objectToReturn)
     {
-
+        poolDictionary[tag].Enqueue(objectToReturn);
     }
 }
