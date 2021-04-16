f0cc8690add6d2b6579da806a3f727aa102eaa25
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2021-04-16 16:10:48 -0500

    Created ship and laser objects

--- UnityProjects/SpringAssignment10/Assets/Scripts/ObjectPooler.cs
+++ UnityProjects/SpringAssignment10/Assets/Scripts/ObjectPooler.cs
@@ -6,5 +6,37 @@ public class ObjectPooler : MonoBehaviour
 {
     public List<Pool> pools;
     public Dictionary<string, Queue<GameObject>> poolDictionary;
-    
+
+    #region Singleton code
+    public static ObjectPooler instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.LogError("Trying to instantiate a second" +
+                "instance of singleton Object Pooler");
+        }
+    }
+    #endregion
+
+    public void FillPoolsWithInactiveObjects()
+    {
+
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        return null;
+    }
+
+    public void ReturnObjectToPool(string tag, GameObject objectToReturn)
+    {
+
+    }
 }
